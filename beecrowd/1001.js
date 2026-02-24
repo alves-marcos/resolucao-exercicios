aebc22f2dcacebeaebfcccbfc3c974bf36be6d68
Marcos Alves <260161654+alves-marcos@users.noreply.github.com>
2026-02-23 22:03:16 -0300

    Primeiros Exercicios enviados /  beecrowd

---
+++ beecrowd/1001.js
@@ -0,0 +1,5 @@
+
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+
+console.log('X = ' + (a+b));
---
+++ beecrowd/1004.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let a = Number(lines.shift());
+let b = Number(lines.shift());
+
+let PROD = a * b
+
+console.log(`PROD = ${PROD}`)
